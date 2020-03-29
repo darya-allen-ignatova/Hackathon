37b19a40cafa79b68d3e3ab9ca77158dabc596d7
Darya Ignatova <darya.ignatova.99@mail.ru>
2020-03-29 12:05:30 +0300

    H2 event list view model with view

---
+++ DI.EventSetup.UI/Controls/EventListView/EventViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DI.EventSetup.Business.ReadModels;
+using DI.EventSetup.UI.Configuration;
+using System;
+
+namespace DI.EventSetup.UI.Controls.EventListView
+{
+    public class EventViewModel : EntityModel<EventModel>
+    {
+        public EventViewModel(EventModel ev) : base(ev)
+        {
+        }
+
+        public int Id => Model.Event.Id;
+
+        public string Name => Model.Event.Name;
+
+        public string Direction => Model.Direction.Name;
+
+        public DateTime Date => Model.Event.StartDateTime.Date;
+
+        public byte[] Logo => Model.Logo.Data;
+    }
+}
