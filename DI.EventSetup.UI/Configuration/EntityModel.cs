79327ea9e12770b94b45e501c857fd5a91a33e4b
Darya Ignatova <darya.ignatova.99@mail.ru>
2020-03-29 12:05:17 +0300

    H2 wrap for view models

---
+++ DI.EventSetup.UI/Configuration/EntityModel.cs
@@ -0,0 +1,20 @@
+﻿using Caliburn.Micro;
+
+namespace DI.EventSetup.UI.Configuration
+{
+    public class EntityModel<T> : Screen
+    {
+        private T _model;
+
+        public EntityModel(T entity)
+        {
+            Model = entity;
+        }
+
+        protected T Model
+        {
+            get => _model;
+            set => Set(ref _model, value);
+        }
+    }
+}
