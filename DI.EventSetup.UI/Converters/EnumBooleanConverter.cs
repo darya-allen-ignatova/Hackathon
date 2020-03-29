3766eecb82d4c91b6d7e9aadfc951fdb0989bea4
Darya Ignatova <darya.ignatova.99@mail.ru>
2020-03-29 12:05:48 +0300

    H2 Main converters for view

---
+++ DI.EventSetup.UI/Converters/EnumBooleanConverter.cs
@@ -0,0 +1,44 @@
+﻿namespace DI.EventSetup.UI.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Windows.Data;
+
+    public class EnumBooleanConverter : IValueConverter
+    {
+        #region IValueConverter Members
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+            {
+                return false;
+            }
+
+            string checkValue = value.ToString();
+            string targetValue = parameter.ToString();
+            return checkValue.Equals(targetValue, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+            {
+                return null;
+            }
+
+            bool useValue = (bool)value;
+            string targetValue = parameter.ToString();
+
+            Type genericArgumentType = null;
+            if (targetType.IsGenericType && targetType.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                genericArgumentType = targetType.GetGenericArguments().First();
+            }
+
+            return useValue ? Enum.Parse(genericArgumentType ?? targetType, targetValue) : null;
+        }
+
+        #endregion
+    }
+}
