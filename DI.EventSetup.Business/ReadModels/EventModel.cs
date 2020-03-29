2cc47dc63b64db8af5edbe1c26ced0c147ad3858
Darya Ignatova <darya.ignatova.99@mail.ru>
2020-03-29 12:03:53 +0300

    H2 Event full model

---
+++ DI.EventSetup.Business/ReadModels/EventModel.cs
@@ -0,0 +1,15 @@
+﻿using DI.EventSetup.Business.Models;
+
+namespace DI.EventSetup.Business.ReadModels
+{
+    public class EventModel
+    {
+        public Event Event { get; set; }
+
+        public Direction Direction { get; set; }
+
+        public City City { get; set; }
+
+        public File Logo { get; set; }
+    }
+}
